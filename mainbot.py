30e73374cfe5f9752aa032a9b8fe306b878ad311
eodbrent <33612870+eodbrent@users.noreply.github.com>
2018-04-03 20:41:56 -0600

    Update mainbot.py

--- mainbot.py
+++ mainbot.py
@@ -10,69 +10,48 @@ import nanexWrap
 import cryptopiaWrap
 import CoinMarketCap
 import coinexchangeWrap
+import stocksExchangeWrap
+import coindeskWrap
+import cryptobridgeWrap
+import tradesatoshiWrap
 import os
 
 Client = discord.Client()
-client = commands.Bot(command_prefix = "?")
-#TODO COINMARKETCAP TOP 5
-#
-#TODO LOG/EXCHANGE SUGGESTIONS +log
-#
-#TODO CLEAN-UP COMMENTS
-
+client = commands.Bot(command_prefix = "$")
+# TODO CHECKLIST FOR ADDING NEW EXCHANGES
+#   ADD to: sptExch list in async def on_message
+#   ADD to: wrap in def searchCoin
+#   ADD to: def formPair
+#   ADD to: fix up wrapper
 @client.event
 async def on_ready():
     print("BOT IS RUNNING!")
     #
     # start logging /primarily for adding new coins or exchanges
     #   from requests !add
-
 @client.event
 async def on_message(message):
+
     msg = message.content.lower()
     split = msg.split(' ')
     if message.author == client.user or isinstance(message.channel, discord.PrivateChannel):
         return
-
-    if msg == 'gfy':
-        await client.send_message(message.channel, "Yup, GFY")
-
-    #read messages
+    sptExch = ["binance", "bittrex", "coinexchange", "cryptopia", "cryptobridge", "kucoin", "nanex", "poloniex", "tradesatoshi", "yobit"]
     if msg.startswith('.'):
-        #this is what we want, so split it.  Don't do anything otherwise.
-        sp = split[0:]
-        #await client.send_message(message.channel, "first = "+sp[0]+" second = "+sp[1])
-        pair = sp[0]
-        pair = pair[1:5]
-        curr = sp[1]
-        if pair == "pair":
-            #await client.send_message(message.channel, "Your chosen pair is " + curr)
-            pair = curr.upper()
-            try:
-                exch = sp[2]
-                #await client.send_message(message.channel, "Your preferred exchange is " + exch)
-            except:
-                exch = 'bittrex'
-                #await client.send_message(message.channel, "No exchange chosen, default is bittrex.")
-            try:
-                pairMsg = coinData(pair, exch)
-                await client.send_message(message.channel, embed=pairMsg)
-            except:
-                errormsg = formError(pair + " does not exist on " + exch + ".\n\t`Check your pair format.  Primary first (BTC_LTC)`")
-                await client.send_message(message.channel, errormsg)
-                return
-            #error = formError("Wrong format")
-        else:
-            await client.send_message(message.channel, "Unsuccessful: " + pair + " is not pair")
-            await client.send_message(message.channel, "Checking command...")
-    #elif msg.startswith('!'):
-    #    sp = split[0:]
-    #    coin = sp[0]
-    #    coin = coin[1:].upper()
-    #    await client.send_message(message.channel, "Going to search for " + coin)
-    #    coinMsg = findCoin(coin)
-    #    await client.send_message(message.channel, coinMsg)
-    elif msg.startswith('-'):
+        splitmsg = msg.split(" ")
+        originalPair = splitmsg[0][1:]
+        if len(splitmsg) > 1:
+            i = 0
+            while i <= len(sptExch):
+                if splitmsg[1] == sptExch[i]:
+                    found = searchCoin(originalPair, sptExch, sptExch.index(splitmsg[1]), None)
+                    break
+                i += 1
+        elif len(splitmsg) <= 1:
+            found = searchCoin(originalPair, sptExch, 0, None)
+
+        await client.send_message(message.channel, embed=found)
+    elif msg.startswith('?'):
         sp = split[0:]
         hp = sp[0]
         hp = hp[1:].lower()
@@ -82,94 +61,34 @@ async def on_message(message):
         if hp == "cmc":
             cmcMsg = CoinMarketCap.coinStats()
             await client.send_message(message.channel, embed=cmcMsg)
-def coinData(pair, exch):
-    exch = exch.lower()
-    pair = pair.replace("-", "_")
+
+def searchCoin(pair, sptExch, num, fiat):
+    wrap = [binWrap, bitWrap, coinexchangeWrap, cryptopiaWrap, cryptobridgeWrap, kucoinWrap, nanexWrap, poloWrap, tradesatoshiWrap, yobitWrap]
+    pair = pair.upper()
     pairsplit = pair.split("_")
-    samp = pairsplit[0]
 
-    print ("sample is: " + samp)
+    # CHECK First currency.  If it's a FIAT (found in coindesk), fix the right parameters and recursive call
+    cdCurrencies = coindeskWrap.getSupportedCurrencies()
+    if pairsplit[0] in cdCurrencies:
+        fiat = pairsplit[0]
+        pair = "BTC" + "_" + pairsplit[1]
+        searchCoin(pair, sptExch, num, fiat)
 
-    if samp == 'BTC':
-        if exch == 'binance':
-            pairFmt = formPair(exch, pair)
-            ticker = binWrap.getTickerData(pairFmt)
-            if ticker:
-                coinMsg = binWrap.getTickerMessage(ticker, pairFmt)
-                return coinMsg
-            else:
-                return formError("No return from server")
-        elif exch == 'bittrex':
-            pairFmt = formPair(exch, pair)
-            ticker = bitWrap.getTickerData(pairFmt)
-            if ticker:
-                coinMsg = bitWrap.getTickerMessage(ticker, pairFmt)
-                return coinMsg
-            else:
-                return formError("No return from server")
-        elif exch == "coinexchange":
-            pairFmt = formPair(exch, pair)
-            ticker = coinexchangeWrap.getTickerData(pairFmt)
-            if ticker:
-                coinMsg = coinexchangeWrap.getTickerMessage(ticker, pairFmt)
-                return coinMsg
-        elif exch == 'cryptopia':
-            pairFmt = formPair(exch, pair)
-            ticker = cryptopiaWrap.getTickerData(pairFmt)
-            if ticker:
-                coinMsg = cryptopiaWrap.getTickerMessage(ticker, pairFmt)
-                return coinMsg
-            else:
-                return formError("No return from server")
-        elif exch == 'kucoin':
-            pairFmt = formPair(exch, pair)
-            ticker = kucoinWrap.getTickerData(pairFmt)
-            if ticker:
-                coinMsg = kucoinWrap.getTickerMessage(ticker, pairFmt)
-                return coinMsg
-            else:
-                return formError("No return from server")
-        elif exch == 'nanex':
-            pairFmt = formPair(exch, pair)
-            ticker = nanexWrap.getTickerData(pairFmt)
-            if ticker:
-                coinMsg = nanexWrap.getTickerMessage(ticker, pairFmt)
-                return coinMsg
-            else:
-                return formError("No return from server")
-        elif exch == 'poloniex':
-            pairFmt = formPair(exch, pair)
-            ticker = poloWrap.getTickerData(pairFmt)
-            if ticker:
-                coinMsg = poloWrap.getTickerMessage(ticker, pairFmt)
-                return coinMsg
-        elif exch == 'yobit':
-            pairFmt = formPair(exch, pair)
-            ticker = yobitWrap.getTickerData(pairFmt)
-            if ticker:
-                coinMsg = yobitWrap.getTickerMessage(ticker, pairFmt)
-                return coinMsg
-            else:
-                return formError("No return from server")
-    elif exch == 'kraken':
-        pairFmt = formPair(exch, pair)
-        ticker = krakenWrap.getTickerData(pairFmt)
-        if ticker:
-            coinMsg = krakenWrap.getTickerMessage(ticker, pairFmt)
-            return coinMsg
-        else:
-            return formError("No return from server")
-    elif exch != 'binance' or exch!= 'bittrex' or exch != "cryptopia" or exch != "kraken" or exch != 'kucoin' or exch != "nanex" or exch != 'poloniex' or exch != "yobit":
-        pairFmt = formPair("bittrex", pair)
-        ticker = bitWrap.getTickerData(pairFmt)
-        if ticker:
-            coinMsg = bitWrap.getTickerMessage(ticker, pairFmt)
-            return coinMsg
-    else:
-        return formError("Currency not found")
+    i = num
+    while i <= len(sptExch):
+        fmtPair = formPair(sptExch[i], pair)
+        try:
+            result = wrap[i].getTickerData(fmtPair)
+            if result:
+                found = wrap[i].getTickerMessage(result, fmtPair, fiat)
+                return found
+        except:
+            print("not on that exchange")
+        i += 1
+    return formError(pair + " is not found on exchanges supported by this bot - weird, cause it has a lot!\n"
+                            "or, something went terribly wrong and logging isn't built in yet.")
 
-#USERS WILL INPUT BTC_LTC as a format.  Don't restrict the user.
-# This function formats the pair for the given exchange and returns it to be used in the ticker.
+
 def formPair(exch, pair):
     if exch == "binance": # ex LTCBTC
         pairsplit = pair.split("_")
@@ -183,6 +102,11 @@ def formPair(exch, pair):
     elif exch == "coinexchange": # ex BTC_LTC (Just need tickercode = LTC)
         pairsplit = pair.split("_")
         pair = pairsplit[1]
+        pair = pair.upper()
+        return pair
+    elif exch == "cryptobridge": # ex LTC_BTC
+        pairsplit = pair.split("_")
+        pair = pairsplit[1] + "_" + pairsplit[0]
         pair = pair.upper()
         return pair
     elif exch == "cryptopia": # ex LTC/BTC
@@ -212,6 +136,16 @@ def formPair(exch, pair):
         # THIS IS HOW USERS WILL INPUT...do nothing / except ensure uppercase
         pair = pair.upper()
         return pair
+    elif exch == "stocksexchange": # ex LTC_BTC
+        pairsplit = pair.split("_")
+        pair = pairsplit[1] + "_" + pairsplit[0]
+        pair = pair.upper()
+        return pair
+    elif exch == "tradesatoshi": # ex LTC_BTC
+        pairsplit = pair.split("_")
+        pair = pairsplit[1] + "_" + pairsplit[0]
+        pair = pair.upper()
+        return pair
     elif exch == "yobit": # ex ltc_btc
         pair = pair.replace("-", "_")
         pairsplit = pair.split("_")
@@ -221,32 +155,22 @@ def formPair(exch, pair):
     else:
         return None
 
-
-def findCoin(coin):
-    pair = "BTC_" + coin
-    # if no exchange chosen, bittrex is default
-    ticker = bitWrap.getTickerData(pair)
-    if ticker:
-        return bitWrap.getTickerMessage(ticker, pair)
-    else:
-        return formError(coin + " is NOT a correct pair")
-
 def help():
     title = "Assistance - AKA Help"
     intro = "You have asked for help, and you shall have it"
     embed = discord.Embed(title=title, description=intro, color=0x0000ff)
 
-    exchanges = "The exchanges currently supported are: `Binance`, `Bittrex`, `CoinExchange`, `Cryptopia`, `Kraken`, `Kucoin`, `Nanex`, `Poloniex`, `YObit`\n\n"
+    exchanges = "Supported exchanges: `Binance`, `Bittrex`, `CryptoBridge`, `Cryptopia`, `Kucoin`, `Nanex`, `Poloniex`, `TradeSatoshi`, `YoBit`\n\n"
     embed.add_field(name="Supported Exchanges", value=exchanges)
 
-    cmdCoin = "`.pair <currency pair> <exchange>` - Returns market data for the specified coin/exchange.\n\n\t- Example: `^pair BTC_LTC Poloniex`\n" \
-              "\t- Note: Some exchanges require a different pair combo (LTC_BTC)\n"
-    cmdHelp = "\t`-help` - Returns information about Tales-From-the-Cryptos.\n"
-    cmdCmc = "\t`-cmc` - Returns the top 5 coins on CoinMarketCap\n\n"
+    cmdCoin = "`.pair` - Returns market data for the specified coin.\t (ex:`.btc_ltc`)\n" \
+              "\t- Fiat is now supported, `.gbp_ltc` will return Litecoin value in British Pound Sterling\n" \
+              "\t- Additionally, you may request an exchange `.btc_ltc cryptopia`\n" \
+              "\t- Note: if an exchange is not specified, see supported exchange list, for exchange\n" \
+              "\t\tsearch priority\n"
+    cmdHelp = "\t`?help` - Returns information about Tales-From-the-Cryptos.\n"
+    cmdCmc = "\t`?cmc` - Returns the top 5 coins on CoinMarketCap\n\n"
     embed.add_field(name="Supported Commands", value=cmdCoin + cmdHelp + cmdCmc)
-
-    # lookup = """For simple currency information, !coin will return data on the single coin."""
-    # embed.add_field(name="Single Currency Lookup", value=lookup)
 
     github = "This project is a work in progress and was derived from Satoshi bot, found at https://github.com/cmsart/Satoshi`\n"
     thanks = "-Thanks for helping me open the door to PYTHON.-"
@@ -256,9 +180,7 @@ def help():
 
 #format error function
 def formError(error):
-    embed = discord.Embed(title="ERROR", description=error, color=0xFF0000)
-    embed.set_footer(text="Ask for help numbnuts (-help)")
-    footer = "Ask for help numbnuts (-help)"
+    footer = "Logging coming when a write/server option can be figured out."
     nextTry = "```prolog\nERROR: " + error + "\n" + footer + "\n```"
     return nextTry
 
