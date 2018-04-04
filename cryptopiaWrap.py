5a7fcb3f9e02838a9abfc57a1a8fda174dde1ad7
eodbrent <33612870+eodbrent@users.noreply.github.com>
2018-04-03 20:45:30 -0600

    Update cryptopiaWrap.py

--- cryptopiaWrap.py
+++ cryptopiaWrap.py
@@ -1,6 +1,7 @@
 import requests
 import discord
 import CoinMarketCap
+import coindeskWrap
 #https://www.cryptopia.co.nz/api/GetMarket/DOT_BTC
 
 def getTickerData(pair):
@@ -12,21 +13,28 @@ def getTickerData(pair):
 
     return None
 
-# Returns formatted market data for the bot to send
-def getTickerMessage(ticker, pair):
+def getTickerMessage(ticker, pair, fiat):
     coin = CoinMarketCap.getReadableCoinName(pair.split("_")[0])
 
-    header = coin + " (" + pair.split("_")[0] + ") - Cryptopia"
-    price = "Current Price: `" + "{:.8f}".format(ticker["LastPrice"]) + "`\n"
-    high = "24hr High: `" + "{:.8f}".format(ticker["High"]) + "`\n"
-    low = "24hr Low: `" + "{:.8f}".format(ticker["Low"]) + "`\n"
-    volume = "24hr Volume: `" + "{:.8f}".format(ticker["Volume"]) + "`\n"
+    price = "Current Price: `" + "{:.8f}".format(ticker["LastPrice"])
+    if fiat:
+        fiatConv = coindeskWrap.getTickerData(fiat)
+        fin = float(ticker["LastPrice"]) * float(fiatConv["rate_float"])
+        fin = fin
+        fiatDisplay = " / {:.2f}".format(fin) + " " + fiat + "`\n"
+    else:
+        btc = coindeskWrap.getTickerData("USD")
+        fin = float(ticker["LastPrice"]) * btc["rate_float"]
+        fiatDisplay = " / {:.2f}".format(fin) + " USD`\n"
+
+    final = price + fiatDisplay
+    header = coin + " (" + pair.split("_")[0] + ")"
 
     changeNum = ticker["Change"]
     sign = "+" if changeNum > 0 else ""
     change = "Percent Change: ```diff\n" + sign + str(changeNum) + "%```"
 
-    data = price + volume + high + low + change
+    data = final + change
 
     if changeNum < 0:
         col = 0xFF0000
@@ -34,6 +42,6 @@ def getTickerMessage(ticker, pair):
         col = 0x00ff00
 
     embed = discord.Embed(title = header, description = data, color = col)
-    embed.set_footer(text = "")
+    embed.set_footer(text = "via Cryptopia | ?help for more bot info")
 
     return embed
