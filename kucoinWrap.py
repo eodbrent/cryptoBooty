946d2e89118f6948c0a9c206273498d45016793e
eodbrent <33612870+eodbrent@users.noreply.github.com>
2018-04-03 20:45:51 -0600

    Update kucoinWrap.py

--- kucoinWrap.py
+++ kucoinWrap.py
@@ -2,6 +2,7 @@
 import requests
 import discord
 import CoinMarketCap
+import coindeskWrap
 import re
 
 # data: "coinType":"XRB"
@@ -15,30 +16,35 @@ def getTickerData(pair):
     return None
 
 # Returns formatted market data for the bot to send
-# TODO PUT IN DOLLAR VALUE
-def getTickerMessage(ticker, pair):
+def getTickerMessage(ticker, pair, fiat):
 
     coin = CoinMarketCap.getReadableCoinName(ticker["coinType"])
+    btc = coindeskWrap.getTickerData("USD")
 
-    header = coin + " (" + ticker["coinType"] + ") - Kucoin"
-    price = "Current Price: `" + "{:.8f}".format(ticker["lastDealPrice"]) + "`\n"
-    high = "24hr High: `" + "{:.8f}".format(ticker["high"]) + "`\n"
-    low = "24hr Low: `" + "{:.8f}".format(ticker["low"]) + "`\n"
-    volume = "24hr Volume: `" + "{:.8f}".format(ticker["vol"]) + "`\n"
+    price = "Current Price: `" + "{:.8f}".format(ticker["lastDealPrice"])
+    if fiat:
+        fiatConv = coindeskWrap.getTickerData(fiat)
+        fin = float(ticker["lastDealPrice"]) * float(fiatConv["rate_float"])
+        fiatDisplay = " / {:.2f}".format(fin) + " " + fiat + "`\n"
+    else:
+        fin = ticker["lastDealPrice"] * btc["rate_float"]
+        fiatDisplay = " / {:.2f}".format(fin) + " USD`\n"
 
-    changeNum = float(ticker["changeRate"])  ###changeRate * 100
-    changeNum = changeNum * 100
+    final = price + fiatDisplay
+    header = coin + " (" + ticker["coinType"] + ")"
+
+    changeNum = float(ticker["changeRate"]) * 100
     sign = "+" if changeNum > 0 else ""
     change = "Percent Change: ```diff\n" + sign + "{:.2f}".format(changeNum) + "%```"
 
-    data = price + volume + high + low + change
+    data = final + change
 
     if changeNum < 0:
         col = 0xFF0000
     elif changeNum > 0:
         col = 0x00ff00
 
-    embed = discord.Embed(title = header, description = data, color = col) #col
-    embed.set_footer(text = "")
+    embed = discord.Embed(title = header, description = data, color = col)
+    embed.set_footer(text = "via Kucoin | ?help for more bot info")
 
     return embed
